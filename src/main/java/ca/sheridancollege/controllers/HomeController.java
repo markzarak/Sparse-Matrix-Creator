b9c1675b5d75604aead909a9baa1b1f1a3d4fdd4
markzarak <zarak@sheridancollege.ca>
2020-10-27 02:55:05 -0400

    Implemented array selection, printing and reading from file.

--- src/main/java/ca/sheridancollege/controllers/HomeController.java
+++ src/main/java/ca/sheridancollege/controllers/HomeController.java
@@ -6,13 +6,19 @@
 
 package ca.sheridancollege.controllers;
 
-import java.text.DecimalFormat;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import ca.sheridancollege.beans.Matrix;
@@ -30,117 +36,155 @@ public class HomeController {
 
 	// Create sparse matrix
 	@GetMapping("/view")
-	public String doAddSparseMatrix(Model model, @RequestParam double rangeMin, @RequestParam double rangeMax,
-			@RequestParam int mRows, @RequestParam int nColumns, @RequestParam double precisionThreshold) {
+	public String doAddSparseMatrix(Model model, @ModelAttribute Matrix matrix, @RequestParam double rangeMin,
+			@RequestParam double rangeMax, @RequestParam int mRows, @RequestParam int nColumns,
+			@RequestParam double precisionThreshold, @RequestParam String compressedSparseRow) {
 
-		model.addAttribute("matrix", new Matrix());
-		Random rand = new Random();
+		// Assign default row and column values for array if none given by user
+		boolean displayDefault = false;
+		if (mRows == 0 && nColumns == 0) {
+			mRows = 4;
+			nColumns = 5;
+			displayDefault = true;
+		}
 
 		// Create empty array for sparse matrix
 		double[][] sparseMatrix = new double[mRows][nColumns];
 
-		System.out.println("***Sparse Matrix A***");
-		// Fill sparse matrix based on user input
-		for (int floor = 0; floor < mRows; ++floor) {
-			for (int room = 0; room < nColumns; ++room) {
-				// Probability of adding 0 based on user threshold value
-				double probability = rand.nextDouble();
-				if (probability <= precisionThreshold) {
-					sparseMatrix[floor][room] = 0;
-					DecimalFormat decimalFormat=new DecimalFormat("#");
-					System.out.print(decimalFormat.format(sparseMatrix[floor][room]) + " ");
-				} else {
-					// If 0 was not added, then add randomly generated number
-					double randomNumber = rangeMin + (rangeMax - rangeMin) * rand.nextDouble();
-					sparseMatrix[floor][room] = randomNumber;
-					System.out.print(String.format("%.0f. ", +sparseMatrix[floor][room]));
+		// Create default sparse matrix from text file if user leaves rows and columns as 0
+		if (displayDefault) {
+			try {
+				File myObj = new File("SparseMatrixInput.txt");
+				Scanner myReader = new Scanner(myObj);
+				int[] fileData = new int[25];
+				int i = 0;
+				while (myReader.hasNextInt()) {
+					fileData[i] = myReader.nextInt();
+					i++;
 				}
-			}
-			System.out.println("");
-		}
-		System.out.println("");
-		
-		// Add sparseMatrix array to model to save data
-//		model.addAttribute("generatedElements", sparseMatrix);
-		// Print sparse matrix to text file
-
-		// Create array V
-		System.out.println("***Array V***");
-		ArrayList<Double> arrayV = new ArrayList<>();
-		for (int floor = 0; floor < sparseMatrix.length; ++floor) {
-			for (int room = 0; room < sparseMatrix[floor].length; ++room) {
-				double currentNum = sparseMatrix[floor][room];
-				// Add number to arrayV if not zero, above 1, or below -1
-				if (currentNum != 0 && (currentNum >= 1 || currentNum <= -1)) {
-					arrayV.add(currentNum);
-				}
-				// Round up if not zero, below 1, and above 0 
-				if (currentNum != 0 && (currentNum < 1 && currentNum > 0)) {
-					arrayV.add(Math.ceil(currentNum));
-				}
-				// Round down if not zero, less than 0, and above -1
-				if (currentNum != 0 && (currentNum < 0 && currentNum > -1)) {
-					arrayV.add(Math.floor(currentNum));
-				}
-			}
-		}
-
-		// Print array V
-		for (int i = 0; i < arrayV.size(); ++i) {
-			System.out.print(String.format("%.0f. ", +arrayV.get(i)));
-		}
-		System.out.println("");
-
-		// Create array J
-		System.out.println("");
-		System.out.println("***Array J***");
-		ArrayList<Integer> arrayJ = new ArrayList<>();
-		// indexJ is used to keep track of the beginning of each line in sparseMatrix
-		int indexJ = 0;
-		for (int floor = 0; floor < sparseMatrix.length; ++floor) {
-			for (int room = 0; room < sparseMatrix[floor].length; ++room) {
-				if (sparseMatrix[floor][room] != 0) {
-					arrayJ.add(indexJ);
-				}
-				indexJ++;
-			}
-			// Reset indexJ to count from the beginning of the next line
-			indexJ = 0;
-		}
-
-		// Print array J
-		for (int i = 0; i < arrayJ.size(); ++i) {
-			System.out.print(arrayJ.get(i) + " ");
-		}
-		System.out.println("");
-
-		// Create array I
-		System.out.println("");
-		System.out.println("***Array I***");
-		ArrayList<Integer> arrayI = new ArrayList<>();
-		// indexOfArrayV is used to keep track of the non zero-numbers added to Array V
-		int indexOfArrayV = 0;
-		for (int floor = 0; floor < sparseMatrix.length; ++floor) {
-			boolean found = false;
-			for (int room = 0; room < sparseMatrix[floor].length; ++room) {
-				if (sparseMatrix[floor][room] != 0) {
-					// If a zero-number has been found, allow only first entry per row from sparseMatrix
-					if (found == false) {
-					arrayI.add(indexOfArrayV);
-					found = true;
+				// Default sparse matrix data in SparseMatrixInput.txt begins on second index
+				i = 2;
+				for (int floor = 0; floor < mRows; ++floor) {
+					for (int room = 0; room < nColumns; ++room) {
+						sparseMatrix[floor][room] = fileData[i];
+						i++;
 					}
-					indexOfArrayV++;
+				}
+				myReader.close();
+				System.out.println("Successfully read data from SparseMatrixInput.txt");
+			} catch (FileNotFoundException e) {
+				System.out.println("An error occurred.");
+				e.printStackTrace();
+			}
+		} else {
+			// Fill sparse matrix based on user input
+			for (int floor = 0; floor < mRows; ++floor) {
+				for (int room = 0; room < nColumns; ++room) {
+					// Probability of adding 0 based on user threshold value
+					Random rand = new Random();
+					double probability = rand.nextDouble();
+					if (probability <= precisionThreshold) {
+						sparseMatrix[floor][room] = 0;
+					} else {
+						// If 0 was not added, then add randomly generated number
+						double randomNumber = rangeMin + (rangeMax - rangeMin) * rand.nextDouble();
+						sparseMatrix[floor][room] = randomNumber;
+					}
 				}
 			}
 		}
-		// Point last element in arrayI to the start of a fictitious (m + 1) row
-		arrayI.add(indexOfArrayV);
+		// Add sparseMatrix array to model to save data
+		model.addAttribute("generatedElements", sparseMatrix);
 
-		// Print array I
-		for (int i = 0; i < arrayI.size(); ++i) {
-			System.out.print(arrayI.get(i) + " ");
+		// Print sparse matrix to text file
+		try {
+			FileWriter file = new FileWriter("SparseMatrixOutput.txt", true);
+			BufferedWriter output = new BufferedWriter(file);
+			output.write("***Sparse Matrix A***");
+			output.newLine();
+			for (int floor = 0; floor < sparseMatrix.length; ++floor) {
+				for (int room = 0; room < sparseMatrix[0].length; ++room) {
+					output.write(String.format("%.0f. ", +sparseMatrix[floor][room]));
+				}
+				output.newLine();
+			}
+			output.newLine();
+			output.flush();
+			output.close();
+			System.out.println("Successfully wrote data to SparseMatrixOutput.txt");
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
 		}
-		System.out.println("");
+
+		// Create array based on checkbox selection:
+		// Create array V
+		if (compressedSparseRow.contains("v")) {
+			ArrayList<Double> arrayV = new ArrayList<>();
+			for (int floor = 0; floor < sparseMatrix.length; ++floor) {
+				for (int room = 0; room < sparseMatrix[floor].length; ++room) {
+					double currentNum = sparseMatrix[floor][room];
+					// Add number to arrayV if not zero, above 1, or below -1
+					if (currentNum != 0 && (currentNum >= 1 || currentNum <= -1)) {
+						arrayV.add(currentNum);
+					}
+					// Round up if not zero, below 1, and above 0
+					if (currentNum != 0 && (currentNum < 1 && currentNum > 0)) {
+						arrayV.add(Math.ceil(currentNum));
+					}
+					// Round down if not zero, less than 0, and above -1
+					if (currentNum != 0 && (currentNum < 0 && currentNum > -1)) {
+						arrayV.add(Math.floor(currentNum));
+					}
+				}
+			}
+			// Add array V to model
+			model.addAttribute("arrayVAttribute", arrayV);
+		}
+		
+		// Create array J
+		if (compressedSparseRow.contains("j")) {
+			ArrayList<Integer> arrayJ = new ArrayList<>();
+			// indexJ is used to keep track of the beginning of each line in sparseMatrix
+			int indexJ = 0;
+			for (int floor = 0; floor < sparseMatrix.length; ++floor) {
+				for (int room = 0; room < sparseMatrix[floor].length; ++room) {
+					if (sparseMatrix[floor][room] != 0) {
+						arrayJ.add(indexJ);
+					}
+					indexJ++;
+				}
+				// Reset indexJ to count from the beginning of the next line
+				indexJ = 0;
+			}
+			// Add array J to model
+			model.addAttribute("arrayJAttribute", arrayJ);
+		}
+		
+		// Create array I
+		if (compressedSparseRow.contains("i")) {
+			ArrayList<Integer> arrayI = new ArrayList<>();
+			// indexOfArrayV is used to keep track of the non zero-numbers added to Array V
+			int indexOfArrayV = 0;
+			for (int floor = 0; floor < sparseMatrix.length; ++floor) {
+				boolean found = false;
+				for (int room = 0; room < sparseMatrix[floor].length; ++room) {
+					if (sparseMatrix[floor][room] != 0) {
+						// If a zero-number has been found, allow only first entry per row from
+						// sparseMatrix
+						if (found == false) {
+							arrayI.add(indexOfArrayV);
+							found = true;
+						}
+						indexOfArrayV++;
+					}
+				}
+			}
+			// Point last element in arrayI to the start of a fictitious (m + 1) row
+			arrayI.add(indexOfArrayV);
+			// Add array I to model
+			model.addAttribute("arrayIAttribute", arrayI);
+		}
 
 		return "home.html";
 	}
